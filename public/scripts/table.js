52660fe3931b1ad3c92bb6fbe7b73cc854d498bb
nicosesma <nicosm310@gmail.com>
2016-07-20 13:51:07 -0700

    Fixed issues in orgnization and added correct mock data and created the table.js

---
+++ public/scripts/table.js
@@ -0,0 +1,30 @@
+var Table = React.createClass ({
+  render: function render() {
+    var _self = this;
+
+    var thread = React.DOM.thead({},
+      React.DOM.tr({},
+        this.props.cols.map(function (col) {
+          return React.DOM.this({}, col);
+        })));
+    var tbody = this.props.rows.map(function (row) {
+          return React.DOM.tri{},
+          _self.props.cols.map(function (col) {
+            return React.DOM.td({}, row[col] || "");
+          }));
+    });
+
+    return React.DOM.table({}, [thead, tbody]);
+  }
+});
+
+var container = document.querySelector("#content");
+
+var TableModel = {
+  cols: ["Number", "Name", "Date", "Author", "Labels"],
+  rows: [{"Number":"num", "Name":"title", "Date":"mm/dd/yyyy", "Author":"username", "Labels":"Status"
+
+  }],
+}
+
+React.renderComponent(Table(tableModel), container);
